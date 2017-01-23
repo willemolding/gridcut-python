5cdcbb4cc10943f28d6f2f9f482b3eccac94ae5a
Jiri Borovec <jiri.borovec@fel.cvut.cz>
2017-01-23 18:14:47 +0100

    cleaning and update (build, test, CI)
    * update build & install
    * add req.
    * add CI
    * update tests

--- setup.py
+++ setup.py
@@ -1,6 +1,45 @@
-from distutils.core import setup, Extension
-from os.path import expanduser
-setup(name='gridcut', version='1.0',  \
-      ext_modules=[Extension('gridcut', ['gridcut_wrapper.cpp'],
-      				include_dirs=[expanduser("~/anaconda/lib/python2.7/site-packages/numpy/core/include"),
-      								"code/GridCut-1.1/include/GridCut"])])
+"""
+Building the GridCut wrapper
+
+>>> python setup.py build_ext --inplace
+>>> python setup.py install
+"""
+
+import os
+from distutils.core import setup
+# from Cython.Distutils import build_ext
+from distutils.extension import Extension
+import urllib
+import zipfile
+
+import numpy as np
+
+PACKAGE = 'GridCut-1.3.zip'
+
+# download code
+package = urllib.URLopener()
+package.retrieve('http://gridcut.com/dl/%s' % PACKAGE, PACKAGE)
+# unzip the package
+with zipfile.ZipFile(PACKAGE, 'r') as zip_ref:
+    zip_ref.extractall('code')
+
+# find the latest version
+PATH_CODE = os.path.join('code', 'include', 'GridCut')
+assert os.path.exists(PATH_CODE), 'missing source code'
+# parse version
+VERSION = os.path.splitext(PACKAGE)[0].split('-')[-1]
+
+
+setup(
+    name='gridcut',
+    version=VERSION,
+    # cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension(
+            'gridcut',
+            ['gridcut_wrapper.cpp'],
+            include_dirs=[PATH_CODE],
+            )
+    ],
+    include_dirs=[np.get_include()],
+)
--- test.py
+++ test.py
@@ -1,17 +1,62 @@
+import numpy as np
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 import gridcut
-from numpy import *
-
-source = array([8,0,0,0,0,0,0,0,0],dtype=float32)
-sink = array([0,0,0,0,0,0,0,9,0],dtype=float32)
-up = array([5,1,0,0,0,0,0,0,0])
-down =   array([0,0,4,0,0,0,0,3,0])
-left =   array([0,0,0,0,3,0,0,0,0])
-right =  array([0,0,5,0,0,0,0,0,0])
-
-pw = ones(9,dtype=float32)*8.
-
-#print gridcut.solve_2D_4C(3,3,source,sink,pw,pw,pw,pw,n_threads=1,block_size=4).reshape((3,3))
 
 
-print gridcut.solve_2D_4C_potts(3,3,8,source,sink,n_threads=1,block_size=1).reshape((3,3))
-print gridcut.solve_2D_4C_potts(3,3,8,source,sink,n_threads=1,block_size=1).reshape((3,3))
+SOURCE = np.array([8, 0, 0, 0, 0, 0, 0, 0, 0], dtype=np.float32)
+SINK = np.array([0, 0, 0, 0, 0, 0, 0, 9, 0], dtype=np.float32)
+PW = np.ones(9, dtype=np.float32) * 8.
+# UP = np.array([5, 1, 0, 0, 0, 0, 0, 0, 0])
+# DOWN = np.array([0, 0, 4,0, 0, 0, 0, 3, 0])
+# LEFT = np.array([0, 0, 0, 0, 3, 0, 0, 0, 0])
+# RIGHT = np.array([0, 0, 5, 0, 0, 0, 0, 0, 0])
+
+
+def test_solve_2d_4c_simple(source=SOURCE, sink=SINK, pw=PW):
+    result = gridcut.solve_2D_4C(3, 3, source, sink, pw, pw, pw, pw,
+                                 n_threads=2, block_size=4)
+    assert np.array_equal(result, np.array([0, 0, 0, 0, 0, 0, 1, 1, 1]))
+
+    result = gridcut.solve_2D_4C_potts(3, 3, 8, source, sink,
+                                       n_threads=2, block_size=1)
+    # FIXME: this should be the same as above
+    assert np.array_equal(result, np.array([1, 1, 1, 1, 1, 1, 1, 1, 1]))
+
+
+def test_solve_2d_4c_image(width=100, height=100):
+    """  """
+    img = np.random.randn(height, width)
+    img[int(0.3 * height):int(0.8 * height),
+        int(0.2 * width):int(0.7 * width)] += 2
+    img -= 1
+
+    unary = np.c_[img.reshape(img.size, 1), -img.reshape(img.size, 1)].copy()
+
+    fig = plt.figure(figsize=(unary.shape[-1] * 4, 4))
+    for i in range(unary.shape[-1]):
+        plt.subplot(1, unary.shape[-1], i + 1)
+        plt.imshow(unary[:, i].reshape((height, width)), cmap="gray",
+                   interpolation="nearest")
+    fig.tight_layout(), fig.savefig('./images/grid_unary.png')
+
+    source = img.ravel().astype(np.float32)
+    sink = -img.ravel().astype(np.float32)
+
+    labels = gridcut.solve_2D_4C_potts(height, width, 8, source, sink,
+                                       n_threads=2, block_size=1)
+
+    fig = plt.figure(figsize=(2 * 4, 4))
+    plt.subplot(1, 2, 1), plt.title('image')
+    plt.imshow(img, interpolation="nearest")
+    plt.subplot(1, 2, 2), plt.title('solved labeling')
+    plt.imshow(labels.reshape(height, width), interpolation="nearest")
+    fig.tight_layout(), fig.savefig('./images/grid_labels.png')
+
+
+# if __name__ == '__main__':
+#     test_solve_2d_4c_simple()
+#     test_solve_2d_4c_image()
